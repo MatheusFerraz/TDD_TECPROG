d7834abbce59c6fc442d810845e4b6bb88a2e157
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-10-13 11:01:59 -0300

    Refactoring test

--- src/notas/teste/NoteTest.java
+++ src/notas/teste/NoteTest.java
@@ -21,15 +21,30 @@ public class NoteTest {
 	public void tearDown() throws Exception {
 		
 	}
-
-	@Test
-	public void shouldReturnTheCorrectAverageForExams() {
-		this.grade.setGradeOne(0.0);
+	
+	private void shouldSetValuesForCalculateAverage(final double firstGrade, final double secondGrade) {
+		this.grade.setGradeOne(firstGrade);
+		this.grade.setGradeTwo(secondGrade);
+	}
+	
+	private double shouldRetrieveAverageCalculated(final double firstGrade, final double secondGrade) {
+		shouldSetValuesForCalculateAverage(firstGrade, secondGrade);
 		double gradeOne = this.grade.getGradeOne();
-		this.grade.setGradeTwo(0.0);
 		double gradeTwo = this.grade.getGradeTwo();
 		double average = this.grade.calculateAverage(gradeOne, gradeTwo);
-		assertEquals(0.0,average,0.0);
+		return average;
+	}
+
+	@Test
+	public void shouldReturnTheCorrectAverageForExamsFirstCase() {
+		double average = shouldRetrieveAverageCalculated(0.0, 0.0);
+		assertEquals(0.0,average,0.01);
+	}
+	
+	@Test
+	public void shouldReturnTheCorrectAverageForExamsSecondCase() {
+		double average = shouldRetrieveAverageCalculated(8.0, 6.0);
+		assertEquals(7.0,average,0.01);
 	}
 
 }
